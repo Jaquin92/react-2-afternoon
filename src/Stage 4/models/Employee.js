f979db40c707edc8b4cb6a5269a693f6c95544a7
Jonathan <jaquin92@yahoo.com>
2018-01-29 18:08:43 -0600

    react 2 evening

--- src/Stage 4/models/Employee.js
+++ src/Stage 4/models/Employee.js
@@ -1,9 +1,21 @@
 export default class Employee {
-  // constructor
-  
+  constructor(id, name, phone, title) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.title = title;
+  }
+
+  updateName(str) {
+    this.name = str;
+  }
   // updateName
-
+  updatePhone(str) {
+    this.phone = str;
+  }
   // updatePhone
-
-   // updateTitle
+  updateTitle(str) {
+    this.title = str;
+  }
+  // updateTitle
 }
